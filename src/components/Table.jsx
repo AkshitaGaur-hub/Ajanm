43e05c0ff5e168681541fc4872729c4c7da756f6
AkshitaGaur-hub <gaurakshita407@gmail.com>
2026-06-16 20:55:03 +0530

    update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,13 +4,42 @@ import { FaCalendarCheck, FaEllo, FaHammer, FaObjectGroup, FaRegPaperPlane, FaSt
 function Table() {
     return (
         <>
-            <div className='tableborder text-black'>
-                <div className="plain"><FaObjectGroup className='text-orange-600'/><span id="pp">AI-Personalized Plans</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
-                <div className="plain"><FaHammer className='text-orange-600'/><span id="pp">No Equipment Needed</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
-                <div className="plain"><FaStopwatch className='text-orange-600'/><span id="pp">30 Minutes a Day</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
-                <div className="plain"><FaRegPaperPlane className='text-orange-600'/><span id="pp">90+ Conditions Targeted</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
-                <div className="plain"><FaEllo className='text-orange-600'/><span id="pp">Expert Guidance</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
-                <div className="plain"><FaCalendarCheck className='text-orange-600'/><span id="pp">Track & Transform</span><span id="detail">Lorem ipsum dolor sit amet consectetur.</span></div>
+            <div className='tableborder'>
+                <div className="plain">
+                    <FaObjectGroup className='icon_color' />
+                    <span id="pp">AI-Personalized Plans</span>
+                    <span id="detail">Plans built for your goals.</span>
+                </div>
+
+                <div className="plain">
+                    <FaHammer className='icon_color' />
+                    <span id="pp">No Equipment Needed</span>
+                    <span id="detail">Train anywhere, anytime.</span>
+                </div>
+
+                <div className="plain">
+                    <FaStopwatch className='icon_color' />
+                    <span id="pp">30 Minutes a Day</span>
+                    <span id="detail">Quick and effective sessions.</span>
+                </div>
+
+                <div className="plain">
+                    <FaRegPaperPlane className='icon_color' />
+                    <span id="pp">90+ Conditions Targeted</span>
+                    <span id="detail">Support for diverse needs.</span>
+                </div>
+
+                <div className="plain">
+                    <FaEllo className='icon_color' />
+                    <span id="pp">Expert Guidance</span>
+                    <span id="detail">Trusted professional advice.</span>
+                </div>
+
+                <div className="plain">
+                    <FaCalendarCheck className='icon_color' />
+                    <span id="pp">Track & Transform</span>
+                    <span id="detail">Measure progress easily.</span>
+                </div>
             </div>
         </>
     )
