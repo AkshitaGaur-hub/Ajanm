65494be6a21f3aee29fd084fa764f0798794f383
AkshitaGaur-hub <gaurakshita407@gmail.com>
2026-06-18 10:22:27 +0530

    update

--- src/components/AjanmProgram.jsx
+++ src/components/AjanmProgram.jsx
@@ -3,8 +3,6 @@ import "./Features.css"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import pro from "../assets/images/pro.png"
-import progress1 from "../assets/images/progress1.png"
-import progress2 from "../assets/images/process2.png"
 import num1 from "../assets/images/num1.png"
 import num2 from "../assets/images/num2.png"
 import num3 from "../assets/images/num3.png"
@@ -75,9 +73,7 @@ const AjanmProgram = () => {
                 </div>
 
                 <div className="righths">
-                    <img src={progress1} className='pro1' alt="mobile-left"/>
                     <img src={pro} alt="mobile" className='pro'/>
-                    <img src={progress2} className='pro1' alt="mobile-right" />
                 </div>
             </div>
             <Footer />
